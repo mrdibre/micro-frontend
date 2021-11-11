797331d00f6cee708486f7087c2097708e15421f
César Felipe <cesar.felipe@cvortex.io>
2021-11-11 18:53:24 -0300

    fetching products on home and sharing products modules

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./index.scss";
+
 import Header from "./Header";
 import Footer from "./Footer";
-import "./index.scss";
+import { HomeContent } from "./HomeContent";
 
 const App = () => (
   <div className="text-3xl mx-auto max-w-6xl">
     <Header app={{ name: "Home" }} />
-    <div className="my-10 ">
-      Home Page Content
-    </div>
+    <HomeContent />
     <Footer />
   </div>
 );
+
 ReactDOM.render(<App />, document.getElementById("app"));
---
+++ home/src/products.js
@@ -0,0 +1,10 @@
+const API_SERVER = "http://localhost:8081";
+
+export const getProducts = async () => fetch(`${API_SERVER}/products`).then(res => res.json());
+
+export const getProduct = async (id) => fetch(`${API_SERVER}/products/${id}`).then(res => res.json());
+
+export const currency = new Intl.NumberFormat("pt-BR", {
+  style: "currency",
+  currency: "BRL"
+});
